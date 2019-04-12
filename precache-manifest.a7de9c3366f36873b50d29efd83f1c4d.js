0cef8c77541df5fa7a82250486b035b34b908cd2
Anders Fjeldsø Christiansen <afc@hesehus.dk>
2019-04-12 11:48:37 +0200

    Updates

---
+++ precache-manifest.a7de9c3366f36873b50d29efd83f1c4d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "79aba875086f3a40be40",
+    "url": "/nec/static/js/runtime~main.189327a1.js"
+  },
+  {
+    "revision": "e6578b6d819b3f06c694",
+    "url": "/nec/static/js/main.30c89d11.chunk.js"
+  },
+  {
+    "revision": "1dabbbd5a30b6665a59a",
+    "url": "/nec/static/js/2.9ba19282.chunk.js"
+  },
+  {
+    "revision": "42adc63f6d3f1db6b75e08cb4e0d941e",
+    "url": "/nec/index.html"
+  }
+];
