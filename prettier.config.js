f8377c778bbb8ab747845b8414b5d07b02ee2648
Anders Fjeldsø Christiansen <afc@hesehus.dk>
2019-04-12 11:13:03 +0200

    development

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    tabWidth: 4,
+    printWidth: 120,
+    endOfLine: 'crlf',
+    overrides: [
+        {
+            files: ['**/*.js', '**/*.jsx'],
+            options: {
+                singleQuote: true
+            }
+        }
+    ]
+};
