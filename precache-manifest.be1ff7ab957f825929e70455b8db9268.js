7bf85db533591f7738a933eadf19733af91d3018
Fjeldsoe <fjeldsoe@gmail.com>
2020-07-08 13:37:12 +0200

    Updates

---
+++ precache-manifest.be1ff7ab957f825929e70455b8db9268.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9db7eab18417cfa247d228b789ddd4c6",
+    "url": "/nec/index.html"
+  },
+  {
+    "revision": "59930b83efb875f3429f",
+    "url": "/nec/static/js/2.a24b0949.chunk.js"
+  },
+  {
+    "revision": "ad24fcd838c83125b7677948ad43fdf2",
+    "url": "/nec/static/js/2.a24b0949.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c9412c0fa4b7ae90744",
+    "url": "/nec/static/js/main.3ac9ce08.chunk.js"
+  },
+  {
+    "revision": "56d9aaa150ba3bbca906",
+    "url": "/nec/static/js/runtime-main.894c98f8.js"
+  }
+]);
